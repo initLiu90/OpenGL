13b3515006fa6bf44565d52f4d2d60597c8eb02e
initLiu90 <xianyue_pan@163.com>
2018-08-16 15:01:02 +0800

    day3，顶点增加颜色

---
+++ day3/src/main/java/com/lzp/day3/MainActivity.java
@@ -0,0 +1,53 @@
+package com.lzp.day3;
+
+import android.app.ActivityManager;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    GLSurfaceView mGlSurfaceView;
+    boolean rendererSet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGlSurfaceView = (GLSurfaceView) findViewById(R.id.glsurfaceview);
+
+        if (isSupportOpenGLES2()) {
+            mGlSurfaceView.setEGLContextClientVersion(2);
+            mGlSurfaceView.setRenderer(new AirHockeyRenderer(this));
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, "unsupport open gl es2", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (rendererSet) {
+            mGlSurfaceView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (rendererSet) {
+            mGlSurfaceView.onPause();
+        }
+    }
+
+    private boolean isSupportOpenGLES2() {
+        final ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+        return supportsEs2;
+    }
+}
